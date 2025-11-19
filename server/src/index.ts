eb6b07f37108d1f730ff5749ea83fa5c5f29df09
xwang <xion36325@gmail.com>
2025-11-19 21:33:22 +0530

    update backend %%

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors( {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 let isConnected = false;
